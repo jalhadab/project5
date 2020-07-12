419052174ffdbfb2683cf4381ee61806afb2c6ba
eunlimkim <ekim69@u.rochester.edu>
2020-07-11 22:13:03 -0400

    update kim php

--- kim.php
+++ kim.php
@@ -1,9 +1,52 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
+<?php 
+include "inc/html-top.php";
+?>
 
-</body>
+</head>
+
+		<body class="container">
+			<?php include "inc/nav.php"; ?>
+
+			<header>
+			<h1>Eunlim Kim</h1>
+			<div>Major in Computer Science</div>
+			</header>
+
+			<div class="background">
+			<div class="container">
+			<article>
+				<h2>Eunlim Kim</h2>
+				<p>Visual Designer</p>
+				
+				<h2>Intro</h2>
+					<p>University of Rochester</p>
+					<P>Major: BA Computer Science</P>
+					<p>Class of 2021</p>
+					<p>Hello. I am Eunlim Kim and this is my webpage! This webpage is made for CSC174 and looks very interesting! Hope you have a great day!</p>
+			
+		
+				<h2>Early Years</h2>
+				
+				<p> I was born in Daegu, South Korea and moved to Ulsan. After 7 years in Ulsan, my parents decided to move to Busan for better education. I went elementary school and middle school in Busan then I told my parents that I want to study in the United States. In 2012, I left South Korea and started to study in Rhode Island. I met most of my good friends during that time.</p>
+				
+				<h3>Projects</h3>
+				
+				<p>These are the projects I have done. The most recent one is done with python. For Data base, I used sqlite. This project for people who are searching book near their house.</p>
+					<ul>
+						<li>A project I have done</li>
+						<li>Another proejct I have done</li>
+						<li>And so on ...</li>
+					</ul>
+				
+			</article>
+		<footer>
+            <a href="http://csc174.org/lab01/ekim69/" class="eunlim">Eunlim's Original Lab 1</a>
+			<p>Project 1 - Chicago</p>
+		</footer>
+
+		</div> <!-- center container -->
+	</div><!--  background container -->
+
+		<?php include "inc/scripts.php"; ?>
+	</body>
 </html>
