f027475d4188fa859641f808a2d11c772d65301a
Emely Rosa <erosa4@u.rochester.edu>
2020-07-13 00:43:24 -0400

    emely's page

--- emely.php
+++ emely.php
@@ -1,9 +1,89 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
+<?php 
+include "inc/html-top.php";
+?>
 
+</head>
+
+
+
+<body>
+	<?php include "inc/nav.php"; ?>
+	
+	<header>
+		<h1>Emely Rosa</h1>
+		<div>B.A in Computer Science</div>
+	</header>
+
+
+	<div class="background">
+		<div class="container">
+		<article>
+			<h2>Introduction</h2>
+				<figure class="intro-selfie">
+					<img src="images/emely.jpg" alt="pic of Emely">
+				</figure>
+
+				<div class="intro-container">
+					<p>My name is Emely Rosa-Ortiz. I am a rising senior student in University of Rochester majoring in computer science. I am from New York City. I am passionate about coffee and latte art. </p>
+				</div>
+
+			<h2>Early Years</h2>
+				<p>  My name is Emely Rosa-Ortiz. I am a rising senior student in University of Rochester majoring in computer science. I am from New York City. I am Dominican and Puerto Rican because my mom is from PR and my dad is from DR. I grew up in Brooklyn, NY. However, I did spend a year of my life in the Dominican Republic where I was home schooled. I went to elementry school, middle school, and high school all in Brooklyn.
+				I graduated from Uncommon Charter High School in 2017 with High Honors. Now I am going into hopefully my last year at the UR. </p>
+				<h3>Education background</h3>
+					<ol>
+						<li>
+							2002-2008:
+							<ul><li>P.S. 120</li></ul>
+						</li>
+						<li>
+							2008-2013:
+							<ul><li>Williamsburg Collegiate Charter School</li></ul>
+						</li>
+						<li>
+							2013-2017:
+							<ul><li>Uncommon Charter High School</li></ul>
+						</li>
+						<li>
+							2017-Now:
+							<ul><li>University of Rochester</li></ul>
+						</li>
+					</ol>
+
+
+			<h2>Major Course List</h2>
+				<ol>
+					<li>
+						<ol>
+							<li>CSC 161: INTRO TO PROGRAMMING</li>
+							<li>CSC 171: INTO TO COMPUTER SCIENCE</li>
+							<li>CSC 172: DATA STRUCTURES AND ALGORITHMS</li>
+							<li>CSC 273W: WRITING FOR COMPUTER SCIENCE</li>
+							<li>WRT 273: COMMUNICATING YOUR PROFESSIONAL IDENTITY</li>
+							<li>CSC 170: WEB DESIGN AND DEVELOPMENT</li>
+							<li>CSC 173: COMPUTATION AND FORMAL SYSTEMS</li>
+							<li>CSC 186: VIDEO GAME DEVELOPMENT</li>
+							<li>CSC 174: ADVANCED FRONT-END WEB DEVELOPMENT </li>
+							<li>CSC 214: MOBILE APP DEVELOPMENT</li>
+						</ol>
+					</li>
+				</ol>
+
+
+			<h2>Hobby</h2>
+				<p> I don't get to do my "hobbies" that often. However, I am into latte art. I started liking latte art last summer when I worked at Ralph's
+				Coffee Shop. At first it's pretty tricky, but you get the hang of it. I am not that great. I am also into playing Animal Crossing. </p>
+		</article>
+
+		<footer>
+			<p>CSC 174: Advanced Front-end Web - Project 2</p>
+		</footer>
+
+		</div> <!-- center container -->
+	</div><!--  background container -->
+
+<?php include "inc/scripts.php" ?>
 </body>
+
+
 </html>
