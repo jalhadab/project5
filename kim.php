2b2e21cf6d57a2f74674a79070e7780ef11fe5a5
eunlimkim <ekim69@u.rochester.edu>
2020-07-13 04:31:11 -0400

    kim's page updated and little change in css

--- kim.php
+++ kim.php
@@ -4,7 +4,7 @@ include "inc/html-top.php";
 
 </head>
 
-		<body class="container">
+		<body>
 			<?php include "inc/nav.php"; ?>
 
 			<header>
@@ -15,33 +15,64 @@ include "inc/html-top.php";
 			<div class="background">
 			<div class="container">
 			<article>
-				<h2>Eunlim Kim</h2>
-				<p>Visual Designer</p>
+				<h2>Introduction</h2>
+				<figure class="intro-selfie">
+					<img src="images/eunlimkim.jpg" alt="kim's selfie">
+				</figure>
 				
-				<h2>Intro</h2>
-					<p>University of Rochester</p>
-					<P>Major: BA Computer Science</P>
-					<p>Class of 2021</p>
-					<p>Hello. I am Eunlim Kim and this is my webpage! This webpage is made for CSC174 and looks very interesting! Hope you have a great day!</p>
-			
-		
+				<div class="intro-container">
+					<p>Hello, my name is Eunlim Kim. I am a senior majoring in computer science, and my particular interest is Human Computer Interaction (HCI). I was born in South Korea and came to United States when I was 15 years old. I want to use my knwoledge in HCI to help people who has disadvantages.</p>
+				</div>
+
 				<h2>Early Years</h2>
+				<p> I was born in Daegu, South Korea and moved to Ulsan. After living in Ulsan for 7 years, my parents decided to move to Busan for better education. I went elementary school and middle school in Busan then I told my parents that I want to study in the United States. In 2012, I left South Korea and started to study in Rhode Island. I met most of my good friends during that time.</p>
+
+					<h3>Education background</h3>
+						<ol>
+							<li>
+								2004-2010:
+								<ul><li>Korean Elementary School</li></ul>
+							</li>
+							<li>
+								2010-2011:
+								<ul><li>Korean Middle Shcool </li></ul>
+							</li>
+							<li>
+								2012-2016:
+								<ul><li>Saint Mary Academy in Rhode Island</li></ul>
+							</li>
+							<li>
+								2017-Now:
+								<ul><li>Bachelor of Computer Science at University of Rochester</li></ul>
+							</li>
+						</ol>
+
+						<h2>Major Course List</h2>
 				
-				<p> I was born in Daegu, South Korea and moved to Ulsan. After 7 years in Ulsan, my parents decided to move to Busan for better education. I went elementary school and middle school in Busan then I told my parents that I want to study in the United States. In 2012, I left South Korea and started to study in Rhode Island. I met most of my good friends during that time.</p>
-				
-				<h3>Projects</h3>
-				
-				<p>These are the projects I have done. The most recent one is done with python. For Data base, I used sqlite. This project for people who are searching book near their house.</p>
-					<ul>
-						<li>A project I have done</li>
-						<li>Another proejct I have done</li>
-						<li>And so on ...</li>
-					</ul>
-				
-			</article>
+
+					<li>
+						Univesity of Rochester (2017-2020):
+						<ol>
+							<li>CSC 161: INTRO TO PROGRAMMING</li>
+							<li>CSC 170: INTRO TO WEB DEVELOPMENT</li>
+							<li>CSC 171: INTRO TO COMPUTER SCIENCE</li>
+							<li>CSC 172: DATA STRUCTURES & ALGORITHMS</li>
+							<li>CSC 173: COMPUTATION & FORMAL SYSTEMS</li>
+							<li>CSC 174: ADV FRONT END WEB DEVELOPMNT</li>
+							<li>CSC 186: VIDEO GAME DEVELOPMENT</li>	
+							<li>CSC 210: WEB PROGRAMMING</li>
+							<li>CSC 212: HUMAN COMPUTER INTERACTION</li>
+							<li>CSC 214: MOBILE APP DEVELOPMENT</li>
+							<li>CSC 242: ARTIFICIAL INTELLIGENCE</li>
+						</ol>
+					</li>
+
+					<h2>Hobby</h2>
+				<p>In United States, I usually watch movie with my friends and cook. I love Science Fiction, action, and comic. I try to cook various kind including simple italian food, korean traditional dishes and typical brunch menu. In Korea, I mostly spend time with my dog, Abong (corgi). I usually walk him for at least one hour but he never gets enough of it. I also love trying new restaurants and adding good ones to my favorite restaurant list.</p>
+		</article>
+
 		<footer>
-            <a href="http://csc174.org/lab01/ekim69/" class="eunlim">Eunlim's Original Lab 1</a>
-			<p>Project 1 - Chicago</p>
+			<p>CSC 174: Advanced Front-end Web - Project 2</p>
 		</footer>
 
 		</div> <!-- center container -->
