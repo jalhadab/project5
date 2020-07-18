4c72370dd9d37ea2b636be191b8a1cfeb4bc9274
Michael B <mbashner@u.rochester.edu>
2020-07-18 17:22:48 -0400

    Did the first major pass of the z-page
    The z-page needs to be renamed to index and needs some central image. It also needs a bunch of styling

---
+++ directory.php
@@ -0,0 +1,5 @@
+<?php include "inc/html-top.php"; ?>
+	<body>
+		<?php include "inc/nav.php"; ?>
+	</body>
+</html>
--- inc/html-top.php
+++ inc/html-top.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-
-
-<head>
-	<meta charset="utf-8">
-	<title>Project 1</title>
-	<link rel="stylesheet" href="css/nav.css">
-	<link rel="stylesheet" href="css/style.css">
-	<?php echo $customCSS; ?>
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Project 3 Seoul Team">
+		<title>Project 3</title>
+		<link rel="stylesheet" href="css/nav.css">
+		<link rel="stylesheet" href="css/style.css">
+		<?php echo $customCSS; ?>
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+	</head>
