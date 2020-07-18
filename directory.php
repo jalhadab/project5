05339c0c6ec01f300d45b257e102b261b1d6683b
Evan Pollack <evanmpollack@gmail.com>
2020-07-18 17:31:45 -0400

    Added content based on Michael's f-pattern

---
+++ directory.php
@@ -0,0 +1,50 @@
+<?php include "inc/html-top.php"?>
+
+<body>
+    <header>
+        <h1>CSC 174</h1>
+    </header>
+
+    <?php include "inc/nav.php"?>
+
+    <div class="background">
+        <div class="container">
+            <article>
+                <h2>Our Team</h2>
+
+                <section>
+                    <img src="images/bo-selfie.jpg" alt="Bo">
+
+                    <h3>Bo Wu</h3>
+
+                    <p>Enter Excerpt Here</p>
+
+                    <a href="http://csc174.org/project2/seoul/bo.php">Link to Bo's project 2 page</a>
+                </section>
+
+                <section>
+                    <img src="images/emely.jpg" alt="Emely">
+
+                    <h3>Emely Rosa</h3>
+
+                    <p>Enter Excerpt Here</p>
+
+                    <a href="http://csc174.org/project2/seoul/emely.php">Link to Emely's project 2 page</a>
+                </section>
+
+                <section>
+                    <img src="images/eunlimkim.jpg" alt="Eunlim">
+
+                    <h3>Eunlim Kim</h3>
+
+                    <p>Enter Excerpt Here</p>
+
+                    <a href="http://csc174.org/project2/seoul/kim.php">Link to Eunlim's project 2 page</a>
+                </section>
+            </article>
+        </div>
+    </div>
+
+    <?php include "inc/scripts.php"?>
+</body>
+</html>
