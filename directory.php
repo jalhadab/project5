035b88276bd2a6c009bff0cd7a67dd4da2609bc8
Evan Pollack <evanmpollack@gmail.com>
2020-07-19 15:28:01 -0400

    Added real excerpts and aligned layout

--- directory.php
+++ directory.php
@@ -17,7 +17,10 @@
                     <div>
                         <h3>Bo Wu</h3>
 
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent rutrum, tellus in condimentum pretium, mauris nisl lacinia sapien, vel congue dolor leo id odio. Duis risus massa, ultrices vel consectetur in, luctus sed enim. Duis lacus ex, placerat quis ligula eget, auctor blandit magna. Nam eu ullamcorper odio, vitae dictum metus. Nunc dignissim blandit eleifend. Maecenas pulvinar suscipit condimentum. Sed tristique lorem in urna semper, sed lobortis mi laoreet. Nullam efficitur odio ex, at pellentesque turpis tristique in. Quisque quis dictum lectus.</p>
+                        <p>My name is Bo Wu. I am a rising junior student in University of Rochester majoring in
+                            Computer Science. Before UR, I spend two years of my college life at Saint Louis Unversity
+                            in Missouri. I am also an international student from Anhui, China. I am passionate about
+                            video games and many things associated with games since I was in my primary school.</p>
                     </div>
 
                     <a href="http://csc174.org/project2/seoul/bo.php" target="_blank">Read more</a>
@@ -29,7 +32,8 @@
                     <div>
                         <h3>Emely Rosa</h3>
 
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent rutrum, tellus in condimentum pretium, mauris nisl lacinia sapien, vel congue dolor leo id odio. Duis risus massa, ultrices vel consectetur in, luctus sed enim. Duis lacus ex, placerat quis ligula eget, auctor blandit magna. Nam eu ullamcorper odio, vitae dictum metus. Nunc dignissim blandit eleifend. Maecenas pulvinar suscipit condimentum. Sed tristique lorem in urna semper, sed lobortis mi laoreet. Nullam efficitur odio ex, at pellentesque turpis tristique in. Quisque quis dictum lectus.</p>
+                        <p>My name is Emely Rosa-Ortiz. I am a rising senior student in University of Rochester majoring
+                            in computer science. I am from New York City. I am passionate about coffee and latte art.</p>
                     </div>
 
                     <a href="http://csc174.org/project2/seoul/emely.php" target="_blank">Read more</a>
@@ -41,7 +45,10 @@
                     <div>
                         <h3>Eunlim Kim</h3>
 
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent rutrum, tellus in condimentum pretium, mauris nisl lacinia sapien, vel congue dolor leo id odio. Duis risus massa, ultrices vel consectetur in, luctus sed enim. Duis lacus ex, placerat quis ligula eget, auctor blandit magna. Nam eu ullamcorper odio, vitae dictum metus. Nunc dignissim blandit eleifend. Maecenas pulvinar suscipit condimentum. Sed tristique lorem in urna semper, sed lobortis mi laoreet. Nullam efficitur odio ex, at pellentesque turpis tristique in. Quisque quis dictum lectus.</p>
+                        <p>Hello, my name is Eunlim Kim. I am a senior majoring in computer science, and my particular
+                            interest is Human Computer Interaction (HCI). I was born in South Korea and came to United
+                            States when I was 15 years old. I want to use my knwoledge in HCI to help people who has
+                            disadvantages.</p>
                     </div>
 
                     <a href="http://csc174.org/project2/seoul/kim.php" target="_blank">Read more</a>
