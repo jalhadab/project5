e7a363d2e112e766b721ac9bc279c746b088d49b
Michael B <mbashner@u.rochester.edu>
2020-07-20 18:22:15 -0400

    Update nav.php
    Corrected nav bar to contain a link to the directory

--- inc/nav.php
+++ inc/nav.php
@@ -1,7 +1,7 @@
 <nav class="menu">
 	<ul>
-		<li><a href="z-pattern.php">Z-pattern</a></li>
 		<li><a href="index.php">Home</a></li>
+		<li><a href="directory.php">Directory</a></li>
 		<li><a href="bo.php">Bo Wu</a></li>
 		<li><a href="emely.php">Emely Rosa</a></li>
 		<li><a href="kim.php">Eunlim Kim</a></li>
