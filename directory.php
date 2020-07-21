9bef78f9716a3ec8547a1162f1cd22f7ff94e8af
Michael B <mbashner@u.rochester.edu>
2020-07-21 17:52:57 -0400

    Update directory.php
    Updated the formatting; changed direction of arrow on return home

--- directory.php
+++ directory.php
@@ -1,68 +1,50 @@
 <?php include "inc/html-top.php"?>
-<body>
-    <header>
-        <h1>CSC 174</h1>
-    </header>
-
-    <?php include "inc/nav.php"?>
-
-    <div class="background">
-        <div class="container">
-            <article>
-                <h2>Our Team</h2>
-
-                <section class="dir-student space-below">
-                    <img src="images/bo-selfie.jpg" alt="Bo">
-
-                    <div>
-                        <h3>Bo Wu</h3>
-
-                        <p>My name is Bo Wu. I am a rising junior student in University of Rochester majoring in
-                            Computer Science. Before UR, I spend two years of my college life at Saint Louis Unversity
-                            in Missouri. I am also an international student from Anhui, China. I am passionate about
-                            video games and many things associated with games since I was in my primary school.</p>
-                    </div>
-
-                    <a href="bo.php" target="_blank">Read more</a>
-                </section>
-
-                <section class="dir-student space-below">
-                    <img src="images/emely.jpg" alt="Emely">
-
-                    <div>
-                        <h3>Emely Rosa</h3>
-
-                        <p>My name is Emely Rosa-Ortiz. I am a rising senior student in University of Rochester majoring
-                            in computer science. I am from New York City. I am passionate about coffee and latte art.</p>
-                    </div>
-
-                    <a href="emely.php" target="_blank">Read more</a>
-                </section>
-
-                <section class="dir-student">
-                    <img src="images/eunlimkim.jpg" alt="Eunlim">
-
-                    <div>
-                        <h3>Eunlim Kim</h3>
-
-                        <p>Hello, my name is Eunlim Kim. I am a senior majoring in computer science, and my particular
-                            interest is Human Computer Interaction (HCI). I was born in South Korea and came to United
-                            States when I was 15 years old. I want to use my knwoledge in HCI to help people who has
-                            disadvantages.</p>
-                    </div>
-
-                    <a href="kim.php" target="_blank">Read more</a>
-                </section>
-            </article>
-
-            <a href="index.php" class="a-right">Return Home →</a>
-        </div>
-    </div>
-
-    <footer class="dir-footer">
-        <div>Project 3 - Seoul (TEMP)</div>
-    </footer>
-
-    <?php include "inc/scripts.php"?>
-</body>
+	<body>
+		<header>
+			<h1>CSC 174</h1>
+		</header>
+		<div class="background">
+			<div class="container">
+				<article>
+					<h2>Our Team</h2>
+					<section class="dir-student space-below">
+						<img src="images/bo-selfie.jpg" alt="Bo">
+						<div>
+							<h3>Bo Wu</h3>
+							<p>My name is Bo Wu. I am a rising junior student in University of Rochester majoring in
+								Computer Science. Before UR, I spend two years of my college life at Saint Louis Unversity
+								in Missouri. I am also an international student from Anhui, China. I am passionate about
+							video games and many things associated with games since I was in my primary school.</p>
+						</div>
+						<a href="bo.php">Read more</a>
+					</section>
+					<section class="dir-student space-below">
+						<img src="images/emely.jpg" alt="Emely">
+						<div>
+							<h3>Emely Rosa</h3>
+							<p>My name is Emely Rosa-Ortiz. I am a rising senior student in University of Rochester majoring
+							in computer science. I am from New York City. I am passionate about coffee and latte art.</p>
+						</div>
+						<a href="emely.php">Read more</a>
+					</section>
+					<section class="dir-student">
+						<img src="images/eunlimkim.jpg" alt="Eunlim">
+						<div>
+							<h3>Eunlim Kim</h3>
+							<p>Hello, my name is Eunlim Kim. I am a senior majoring in computer science, and my particular
+								interest is Human Computer Interaction (HCI). I was born in South Korea and came to United
+								States when I was 15 years old. I want to use my knwoledge in HCI to help people who has
+							disadvantages.</p>
+						</div>
+						<a href="kim.php">Read more</a>
+					</section>
+				</article>
+				<a href="index.php" class="a-right">← Return Home</a>
+			</div>
+		</div>
+		<footer class="dir-footer">
+			<div>Project 3 - Seoul (TEMP)</div>
+		</footer>
+		<?php include "inc/scripts.php"?>
+	</body>
 </html>
