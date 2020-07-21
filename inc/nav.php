768a995abc8ea295ce6dbc42f61739ce44e189e7
Michael B <mbashner@u.rochester.edu>
2020-07-21 17:57:37 -0400

    Nav Redo; first fonts; margin fixes
    Redid nav to have only one button and made the button sticky; added the css to use the fonts; made adjustments to the uses of margin and padding so without visual change, the elements all move correctly when window is resized

--- inc/nav.php
+++ inc/nav.php
@@ -1,9 +1,3 @@
 <nav class="menu">
-	<ul>
-		<li><a href="index.php">Home</a></li>
-		<li><a href="directory.php">Directory</a></li>
-		<li><a href="bo.php">Bo Wu</a></li>
-		<li><a href="emely.php">Emely Rosa</a></li>
-		<li><a href="kim.php">Eunlim Kim</a></li>
-	</ul>
+	<a href="directory.php">Return to Directory</a>
 </nav>
