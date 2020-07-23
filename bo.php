5a97d26f8ae4444d3566fb48caf9284e4a218ec6
Michael B <mbashner@u.rochester.edu>
2020-07-23 11:15:53 -0400

    Cleaned up spacing and moved nav
    Cleaned up the spacing to make html more readable and moved nav into the header

--- bo.php
+++ bo.php
@@ -1,87 +1,73 @@
 <?php $customTitle="Bo Wu";?>
 <?php include "inc/html-top.php"?>
-
-<body>
-    <?php $customLink="<a href='directory.php'>Return to Directory</a>";?>
-	<?php include "inc/nav.php"?>
-	
-	<header>
-		<h1>Bo Wu</h1>
-		<div>Major in Computer Science</div>
-	</header>
-
-
-	<div class="background">
-		<div class="container">
-		<article>
-			<!--Introduction h2-->
-				<figure class="intro-selfie">
-					<img src="images/bo-selfie.jpg" alt="Bo's selfie">
-				</figure>
-
-				<!--Introduction paragraph-->
-
-			<h2>Early Years</h2>
-				<p>I was born in Huoshan - a county with a reservoir and a winery. I grow up at bottom of the reservoir. Later, I was sent to a boarding school at Hefei - the captical city in the province. I took my middle school and high school there. After finishing second year of the high school, for some reasons, I quit the school. Then I come to the Unites States. I took 1-year language program at UIS, and 2-year college life in SLU. Finally, I transferred to UR in Spring 2020.</p>
-				<h3>Education background</h3>
+	<body>
+		<header class="container">
+			<div>
+				<h1>Bo Wu</h1>
+				<p>Major in Computer Science</p>
+			</div>
+			<?php include "inc/nav.php"?>
+		</header>
+		<div class="background">
+			<div class="container">
+				<article>
+					<!--Introduction h2-->
+					<figure class="intro-selfie">
+						<img src="images/bo-selfie.jpg" alt="Bo's selfie">
+					</figure>
+					<!--Introduction paragraph-->
+					<h2 class="first-heading">Early Years</h2>
+					<p>I was born in Huoshan - a county with a reservoir and a winery. I grow up at bottom of the reservoir. Later, I was sent to a boarding school at Hefei - the captical city in the province. I took my middle school and high school there. After finishing second year of the high school, for some reasons, I quit the school. Then I come to the Unites States. I took 1-year language program at UIS, and 2-year college life in SLU. Finally, I transferred to UR in Spring 2020.</p>
+					<h3>Education background</h3>
 					<ol>
-						<li>
-							1999-2016:
-							<ul><li>Regular Chinese Schools</li></ul>
+						<li>1999-2016:
+							<ul>
+								<li>Regular Chinese Schools</li>
+							</ul>
 						</li>
-						<li>
-							2016-2017:
-							<ul><li>English as second Language Program in University of Illinions at Springfield</li></ul>
+						<li>2016-2017:
+							<ul>
+								<li>English as second Language Program in University of Illinions at Springfield</li>
+							</ul>
 						</li>
-						<li>
-							2017-2019:
-							<ul><li>Bachelor of Computer Science at Saint Louis University</li></ul>
+						<li>2017-2019:
+							<ul>
+								<li>Bachelor of Computer Science at Saint Louis University</li>
+							</ul>
 						</li>
-						<li>
-							2020-Now:
-							<ul><li>Bachelor of Computer Science at University of Rochester</li></ul>
+						<li>2020-Now:
+							<ul>
+								<li>Bachelor of Computer Science at University of Rochester</li>
+							</ul>
 						</li>
 					</ol>
-
-
-			<h2>Major Course List</h2>
-				<ol>
-					<li>
-						In Saint Louis University (2017-2019):
-						<ol>
-							<li>CSCI 1300: INTRO OBJ PROGRAM</li>
-							<li>CSCI 2100: DATA STRUCTURES</li>
-							<li>CSCI 2300: OBJ-ORIENT SOFTWR</li>
-		 					<li>CSCI 2400: COMPUTER ARCH</li>
-		 					<li>CSCI 3100: ALGORITHMS</li>
-						</ol>
-					</li>
-
-					<li>
-						In Univesity of Rochester (2019-2020):
-						<ol>
-							<li>CSC 170: WEB DESIGN & DEVELOPMENT</li>
-							<li>CSC 173: COMPUTATION & FORMAL SYSTEMS</li>
-							<li>CSC 186: VIDEO GAME DEVELOPMENT</li>
-						</ol>
-					</li>
-				</ol>
-
-
-			<h2>Hobby</h2>
-				<p>In my spare time, gaming is the main entertainment activity. I love these dedicate video games. I had been addicted to it for a long period since the first time I played it during primary school. As I grow up, I start to become more self-discipline and play more and more games. It does help me have my own judgement about what kind of games is fun and good for me. Because of this hobby, I take computer science major in hope of becoming a video game developer in the future. Besides video games, I also pay attention to electronic product including phones, video cards and game consoles. I think the most notable product in 2020 is Playstation 5 and Xbox Series X, which mark the beginning of 4K age.</p>
-		</article>
-
-		<footer>
-			<div>CSC 174: Advanced Front-end Web - Project 3</div>
-		</footer>
-
-		</div> <!-- center container -->
-	</div><!--  background container -->
-
-    <?php include "inc/scripts.php"?>
-</body>
-
-
+					<h2>Major Course List</h2>
+					<ol>
+						<li>In Saint Louis University (2017-2019):
+							<ol>
+								<li>CSCI 1300: INTRO OBJ PROGRAM</li>
+								<li>CSCI 2100: DATA STRUCTURES</li>
+								<li>CSCI 2300: OBJ-ORIENT SOFTWR</li>
+								<li>CSCI 2400: COMPUTER ARCH</li>
+								<li>CSCI 3100: ALGORITHMS</li>
+							</ol>
+						</li>
+						<li>In Univesity of Rochester (2019-2020):
+							<ol>
+								<li>CSC 170: WEB DESIGN & DEVELOPMENT</li>
+								<li>CSC 173: COMPUTATION & FORMAL SYSTEMS</li>
+								<li>CSC 186: VIDEO GAME DEVELOPMENT</li>
+							</ol>
+						</li>
+					</ol>
+					<h2>Hobby</h2>
+					<p>In my spare time, gaming is the main entertainment activity. I love these dedicate video games. I had been addicted to it for a long period since the first time I played it during primary school. As I grow up, I start to become more self-discipline and play more and more games. It does help me have my own judgement about what kind of games is fun and good for me. Because of this hobby, I take computer science major in hope of becoming a video game developer in the future. Besides video games, I also pay attention to electronic product including phones, video cards and game consoles. I think the most notable product in 2020 is Playstation 5 and Xbox Series X, which mark the beginning of 4K age.</p>
+				</article>
+				<footer>
+					<div>CSC 174: Advanced Front-end Web - Project 3</div>
+				</footer>
+			</div> <!-- center container -->
+		</div><!--  background container -->
+		<?php include "inc/scripts.php"?>
+	</body>
 </html>
-
--- emely.php
+++ emely.php
@@ -1,83 +1,67 @@
 <?php $customTitle="Emely Rosa";?>
 <?php include "inc/html-top.php"?>
-
-<body>
-    <?php $customLink="<a href='directory.php'>Return to Directory</a>";?>
-	<?php include "inc/nav.php"?>
-	
-	<header>
-		<h1>Emely Rosa</h1>
-		<div>B.A in Computer Science</div>
-	</header>
-
-
-	<div class="background">
-		<div class="container">
-		<article>
-			<!--Introduction h2-->
-				<figure class="intro-selfie">
-					<img src="images/emely.jpg" alt="pic of Emely">
-				</figure>
-
-				<!--Introduction paragraph-->
-
-			<h2>Early Years</h2>
-				<p>  My name is Emely Rosa-Ortiz. I am a rising senior student in University of Rochester majoring in computer science. I am from New York City. I am Dominican and Puerto Rican because my mom is from PR and my dad is from DR. I grew up in Brooklyn, NY. However, I did spend a year of my life in the Dominican Republic where I was home schooled. I went to elementry school, middle school, and high school all in Brooklyn.
-				I graduated from Uncommon Charter High School in 2017 with High Honors. Now I am going into hopefully my last year at the UR. </p>
-				<h3>Education background</h3>
+	<body>
+		<header class="container">
+			<div>
+				<h1>Emely Rosa</h1>
+				<p>B.A in Computer Science</p>
+			</div>
+			<?php include "inc/nav.php"?>
+		</header>
+		<div class="background">
+			<div class="container">
+				<article>
+					<!--Introduction h2-->
+					<figure class="intro-selfie">
+						<img src="images/emely.jpg" alt="pic of Emely">
+					</figure>
+					<!--Introduction paragraph-->
+					<h2 class="first-heading">Early Years</h2>
+					<p>My name is Emely Rosa-Ortiz. I am a rising senior student in University of Rochester majoring in computer science. I am from New York City. I am Dominican and Puerto Rican because my mom is from PR and my dad is from DR. I grew up in Brooklyn, NY. However, I did spend a year of my life in the Dominican Republic where I was home schooled. I went to elementry school, middle school, and high school all in Brooklyn. I graduated from Uncommon Charter High School in 2017 with High Honors. Now I am going into hopefully my last year at the UR.</p>
+					<h3>Education background</h3>
 					<ol>
-						<li>
-							2002-2008:
-							<ul><li>P.S. 120</li></ul>
+						<li>2002-2008:
+							<ul>
+								<li>P.S. 120</li>
+							</ul>
 						</li>
-						<li>
-							2008-2013:
-							<ul><li>Williamsburg Collegiate Charter School</li></ul>
+						<li>2008-2013:
+							<ul>
+								<li>Williamsburg Collegiate Charter School</li>
+							</ul>
 						</li>
-						<li>
-							2013-2017:
-							<ul><li>Uncommon Charter High School</li></ul>
+						<li>2013-2017:
+							<ul>
+								<li>Uncommon Charter High School</li>
+							</ul>
 						</li>
-						<li>
-							2017-Now:
-							<ul><li>University of Rochester</li></ul>
+						<li>2017-Now:
+							<ul>
+								<li>University of Rochester</li>
+							</ul>
 						</li>
 					</ol>
-
-
-			<h2>Major Course List</h2>
-				<ol>
-					<li>
-						<ol>
-							<li>CSC 161: INTRO TO PROGRAMMING</li>
-							<li>CSC 171: INTO TO COMPUTER SCIENCE</li>
-							<li>CSC 172: DATA STRUCTURES AND ALGORITHMS</li>
-							<li>CSC 273W: WRITING FOR COMPUTER SCIENCE</li>
-							<li>WRT 273: COMMUNICATING YOUR PROFESSIONAL IDENTITY</li>
-							<li>CSC 170: WEB DESIGN AND DEVELOPMENT</li>
-							<li>CSC 173: COMPUTATION AND FORMAL SYSTEMS</li>
-							<li>CSC 186: VIDEO GAME DEVELOPMENT</li>
-							<li>CSC 174: ADVANCED FRONT-END WEB DEVELOPMENT </li>
-							<li>CSC 214: MOBILE APP DEVELOPMENT</li>
-						</ol>
-					</li>
-				</ol>
-
-
-			<h2>Hobby</h2>
-				<p> I don't get to do my "hobbies" that often. However, I am into latte art. I started liking latte art last summer when I worked at Ralph's
-				Coffee Shop. At first it's pretty tricky, but you get the hang of it. I am not that great. I am also into playing Animal Crossing. </p>
-		</article>
-
-		<footer>
-			<div>CSC 174: Advanced Front-end Web - Project 3</div>
-		</footer>
-
-		</div> <!-- center container -->
-	</div><!--  background container -->
-
-    <?php include "inc/scripts.php"?>
-</body>
-
-
+					<h2>Major Course List</h2>
+					<ol>
+						<li>CSC 161: INTRO TO PROGRAMMING</li>
+						<li>CSC 171: INTO TO COMPUTER SCIENCE</li>
+						<li>CSC 172: DATA STRUCTURES AND ALGORITHMS</li>
+						<li>CSC 273W: WRITING FOR COMPUTER SCIENCE</li>
+						<li>WRT 273: COMMUNICATING YOUR PROFESSIONAL IDENTITY</li>
+						<li>CSC 170: WEB DESIGN AND DEVELOPMENT</li>
+						<li>CSC 173: COMPUTATION AND FORMAL SYSTEMS</li>
+						<li>CSC 186: VIDEO GAME DEVELOPMENT</li>
+						<li>CSC 174: ADVANCED FRONT-END WEB DEVELOPMENT </li>
+						<li>CSC 214: MOBILE APP DEVELOPMENT</li>
+					</ol>
+					<h2>Hobby</h2>
+					<p> I don't get to do my "hobbies" that often. However, I am into latte art. I started liking latte art last summer when I worked at Ralph's Coffee Shop. At first it's pretty tricky, but you get the hang of it. I am not that great. I am also into playing Animal Crossing.</p>
+				</article>
+				<footer>
+					<div>CSC 174: Advanced Front-end Web - Project 3</div>
+				</footer>
+			</div> <!-- center container -->
+		</div><!--  background container -->
+		<?php include "inc/scripts.php"?>
+	</body>
 </html>
