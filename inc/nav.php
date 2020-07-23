b3272ce44c33930106647f612ed5131892e3e237
Michael B <mbashner@u.rochester.edu>
2020-07-22 21:18:56 -0400

    Changed the nav bar

--- inc/nav.php
+++ inc/nav.php
@@ -1,3 +1,12 @@
 <nav class="menu">
-	<?php echo $customLink;?>
+	<ul>
+		<li><a href="index.php">Home</a></li>
+		<li class="menubtn"><a href="directory.php">Directory</a>
+			<ul class="submenu">
+				<li><a href="bo.php">Bo Wu</a></li>
+				<li><a href="emely.php">Emely Rosa</a></li>
+				<li><a href="kim.php">Eunlim Kim</a></li>
+			</ul>
+		</li>
+	</ul>
 </nav>
