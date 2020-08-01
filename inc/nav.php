0be4591af6e891e48713669afebd01eb3f565dfe
msquidward17 <67696489+msquidward17@users.noreply.github.com>
2020-08-01 15:32:25 -0400

    Updates to navigation styles

--- inc/nav.php
+++ inc/nav.php
@@ -3,9 +3,11 @@
 		<li><a href="index.php">Home</a></li>
 		<li class="menubtn"><a href="directory.php">Directory</a>
 			<ul class="submenu">
+				<li id="seoul">Team Seoul:</li>
 				<li><a href="bo.php">Bo Wu</a></li>
 				<li><a href="emely.php">Emely Rosa</a></li>
 				<li><a href="kim.php">Eunlim Kim</a></li>
+				<li id="other"><a href="directory.php">Other students</a></li>
 			</ul>
 		</li>
 	</ul>
