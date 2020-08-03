1843b5a650b333be356b789683de13a81b5bda38
pseudoCalibrator <leahgoodwin10@gmail.com>
2020-08-03 13:38:12 -0400

    Update directory.php

--- directory.php
+++ directory.php
@@ -2,112 +2,113 @@
 <?php include "inc/html-top.php" ?>
 
 <body>
-	<header class="container">
-		<div>
-			<a href="index.php" id="homepagelink">
-				<h1>CSC 174</h1>
-				<p>Students</p>
-			</a>
-		</div>
-		<?php include "inc/nav.php" ?>
-	</header>
-	<div class="background">
-		<div class="container">
-			<article>
-				<header class="direct">
-					<h2>Directory</h2>
-				</header>
+  <header class="container">
+    <div>
+      <a href="index.php" id="homepagelink">
+        <h1>CSC 174</h1>
+        <p>Students</p>
+      </a>
+    </div>
+    <?php include "inc/nav.php" ?>
+  </header>
 
-				<?php
-				// connect to the database
-				include('connect-db.php');
+  <div class="background">
+    <div class="container">
+      <article>
+        <header class="direct">
+          <h2>Directory</h2>
+        </header>
 
-				// get results from database
-				$result = mysqli_query($connection, "SELECT * FROM directory");
-				?>
-				<div class="data_table">
-					<table>
-						<tr>
-							<!-- <th>id</th> -->
-							<th>quote</th>
-							<th>name</th>
-							<th>bio</th>
-							<th>link</th>
-							<th colspan="2"><em>Delete/ Edit</em></th>
-						</tr>
-						<?php
-						// loop through results of database query, displaying them in the table
-						while ($row = mysqli_fetch_array($result)) {
-						?>
-							<tr>
-								<!-- <td><?php echo $row['id']; ?></td> -->
-								<td><?php echo $row['quote']; ?></td>
-								<td><?php echo $row['fullname']; ?></td>
-								<td><?php echo $row['bio']; ?></td>
-								<td><?php echo $row['link']; ?></td>
-								<td><a href="edit.php?id=<?php echo $row['id']; ?>">Edit</a></td>
-								<td><a onclick="return confirm('Are you sure you want to delete: <?php echo $row["fullname"]; ?>?')" href="delete.php?id=<?php echo $row['id']; ?>">Delete</a></td>
+        <h3>Team Seoul</h3>
+        <section class="dir-student space-below">
+          <div class="studentquote">"This is a quote too powerful to name."</div>
+          <div>
+            <h4>Bo Wu</h4>
+            <p>My name is Bo Wu. I am a rising junior student in University of Rochester majoring in
+              Computer Science. Before UR, I spend two years of my college life at Saint Louis Unversity
+              in Missouri. I am also an international student from Anhui, China. I am passionate about
+              video games and many things associated with games since I was in my primary school.</p>
+          </div>
+          <a href="bo.php">Read more</a>
+        </section>
+        <section class="dir-student space-below">
+          <div class="studentquote">"This is a quote too powerful to name."</div>
+          <div>
+            <h4>Emely Rosa</h4>
+            <p>My name is Emely Rosa-Ortiz. I am a rising senior student in University of Rochester majoring
+              in computer science. I am from New York City. I am passionate about coffee and latte art.</p>
+          </div>
+          <a href="emely.php">Read more</a>
+        </section>
+        <section class="dir-student">
+          <div class="studentquote">"This is a quote too powerful to name."</div>
+          <div>
+            <h4>Eunlim Kim</h4>
+            <p>Hello, my name is Eunlim Kim. I am a senior majoring in computer science, and my particular
+              interest is Human Computer Interaction (HCI). I was born in South Korea and came to United
+              States when I was 15 years old. I want to use my knowledge in HCI to help people who have
+              disadvantages.</p>
+          </div>
+          <a href="kim.php">Read more</a>
+        </section>
 
-							</tr>
-						<?php
-							// close the loop
-						}
-						?>
-					</table>
-				</div>
+        <h3>Other Teams</h3>
+        <!-- /* feed in other students here from the data form */ -->
+        <section class="dir-student space-below">
 
-				<a href="new.php">Add a new record</a>
+          <?php
+          // connect to the database
+          include('connect-db.php');
+
+          // get results from database
+          $result = mysqli_query($connection, "SELECT * FROM directory");
+          ?>
+          <div class="data_table">
+            <table>
+              <!-- <tr>
+                <th>Quote</th>
+                <th>Name</th>
+                <th>Bio</th>
+                <th>Link</th>
+                <th colspan="2"><em>Delete / Edit</em></th>
+              </tr> -->
+              <?php
+              // loop through results of database query, displaying them in the table
+              while ($row = mysqli_fetch_array($result)) {
+              ?>
+                <section class="dir-student space-below">
+                  <div class="studentquote"><?php echo $row['quote']; ?></div>
+                  <div>
+                    <h4><?php echo $row['fullname']; ?></h4>
+                    <p><?php echo $row['bio']; ?></p>
+                  </div>
+                  <a href="http://<?php echo $row['link']; ?>/" target="_blank">Read more</a>
+                  <a href="edit.php?id=<?php echo $row['id']; ?>">Edit</a></td>
+                  <a onclick="return confirm('Are you sure you want to delete: <?php echo $row["firstname"] . " " . $row["lastname"]; ?>?')" href="delete.php?id=<?php echo $row['id']; ?>">Delete</a>
+                </section>
+                <?php
+                // close the loop
+              }
+              ?>
 
 
-				<h3>Team Seoul</h3>
-				<section class="dir-student space-below">
-					<div class="studentquote">"This is a quote too powerful to name."</div>
-					<div>
-						<h4>Bo Wu</h4>
-						<p>My name is Bo Wu. I am a rising junior student in University of Rochester majoring in
-							Computer Science. Before UR, I spend two years of my college life at Saint Louis Unversity
-							in Missouri. I am also an international student from Anhui, China. I am passionate about
-							video games and many things associated with games since I was in my primary school.</p>
-					</div>
-					<a href="bo.php">Read more</a>
-				</section>
-				<section class="dir-student space-below">
-					<div class="studentquote">"This is a quote too powerful to name."</div>
-					<div>
-						<h4>Emely Rosa</h4>
-						<p>My name is Emely Rosa-Ortiz. I am a rising senior student in University of Rochester majoring
-							in computer science. I am from New York City. I am passionate about coffee and latte art.</p>
-					</div>
-					<a href="emely.php">Read more</a>
-				</section>
-				<section class="dir-student">
-					<div class="studentquote">"This is a quote too powerful to name."</div>
-					<div>
-						<h4>Eunlim Kim</h4>
-						<p>Hello, my name is Eunlim Kim. I am a senior majoring in computer science, and my particular
-							interest is Human Computer Interaction (HCI). I was born in South Korea and came to United
-							States when I was 15 years old. I want to use my knowledge in HCI to help people who have
-							disadvantages.</p>
-					</div>
-					<a href="kim.php">Read more</a>
-				</section>
-				<h3>Other Teams</h3>
-				/* feed in other students here from the data form */
-				<section class="dir-student space-below">
-					<div class="studentquote">"Quote."</div>
-					<div>
-						<h4>Name</h4>
-						<p>biography</p>
-					</div>
-					<a href="link">Read more</a>
-				</section>
-			</article>
-			<footer class="dir-footer">
-				<div>CSC 174: Advanced Front End Web Development - Project 4</div>
-			</footer>
-		</div>
-	</div>
-	<?php include "inc/scripts.php" ?>
+                <!-- <tr>
+                  <td><?php echo $row['quote']; ?></td>
+                  <td><?php echo $row['fullname']; ?></td>
+                  <td><?php echo $row['bio']; ?></td>
+                  <td><?php echo $row['link']; ?></td>
+                  <td><a href="edit.php?id=<?php echo $row['id']; ?>">Edit</a></td>
+                  <td><a onclick="return confirm('Are you sure you want to delete: <?php echo $row["firstname"] . " " . $row["lastname"]; ?>?')" href="delete.php?id=<?php echo $row['id']; ?>">Delete</a></td>
+                </tr> -->
+              
+            </table>
+          </div>
+        </section>
+
+        <div>
+          <br>
+          <a href="new.php">Add a new record</a>
+        </div>
 </body>
 
 </html>
