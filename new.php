b6fc50eea5d41b83963ccb3a461a2f9688c4802a
pseudoCalibrator <leahgoodwin10@gmail.com>
2020-08-03 10:14:10 -0400

    Update new.php

--- new.php
+++ new.php
@@ -7,27 +7,26 @@ include('connect-db.php');
 // check if the form has been submitted. If it has, start to process the form and save it to the database
 if (isset($_POST['submit'])) {
 	// get form data, making sure it is valid
-	$id = mysqli_real_escape_string($connection, htmlspecialchars($_POST['id']));
 	$quote = mysqli_real_escape_string($connection, htmlspecialchars($_POST['quote']));
-    $name = mysqli_real_escape_string($connection, htmlspecialchars($_POST['name']));
+    $fullname = mysqli_real_escape_string($connection, htmlspecialchars($_POST['fullname']));
 	$bio = mysqli_real_escape_string($connection, htmlspecialchars($_POST['bio']));
 	$link = mysqli_real_escape_string($connection, htmlspecialchars($_POST['link']));
 
 
 	// check to make sure both fields are entered
-	if ($quote == '' || $name == '' || $bio == '' || $link == '') {
+	if ($quote == '' || $fullname == '' || $bio == '' || $link == '') {
 		// generate error message
 		$error = 'ERROR: Please fill in all required fields!';
 
 		// if either field is blank, display the form again
-		renderForm($id, $quote, $name, $bio, $link, $error);
+		renderForm($id, $quote, $fullname, $bio, $link, $error);
 
 	} else {
 		// save the data to the database
-		$result = mysqli_query($connection, "INSERT INTO directory (id, quote, name, bio, link) VALUES ('$id', '$quote','$name','$bio','$link')");
+		$result = mysqli_query($connection, "INSERT INTO directory (quote, fullname, bio, email) VALUES ('$quote','$fullname','$bio','$link')");
 
 		// once saved, redirect back to the view page
-		header("Location: index.php");
+		header("Location: directory.php");
 	}
 } else {
 	// if the form hasn't been submitted, display the form
