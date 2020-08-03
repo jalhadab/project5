7e5cfbe5a824a40571218104ee873b13d833b669
pseudoCalibrator <leahgoodwin10@gmail.com>
2020-08-03 13:47:58 -0400

    Update renderform.php

--- renderform.php
+++ renderform.php
@@ -1,71 +1,75 @@
 <?php
 // creates the edit record form
-function renderForm($id, $quote, $fullname, $bio, $link, $error) {
+function renderForm($id, $quote, $fullname, $bio, $link, $error)
+{
 ?>
-<!doctype html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Submit New Record</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
-	<link rel="stylesheet" type="text/css" href="css/override.css">
-</head>
-<body class="formpage">
+	<!doctype html>
+	<html>
 
-<?php
-// if there are any errors, display them
-if ($error != '') {
-	echo '<div style="padding:4px; border:1px solid red; color:red;">'.$error.'</div>';
-}
-?>
-<div class="container">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Submit New Record</title>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
+		<link rel="stylesheet" type="text/css" href="css/override.css">
+	</head>
 
-<h1>Submit New Record</h1>
+	<body class="formpage">
+		<?php
+		// if there are any errors, display them
+		if ($error != '') {
+			echo '<div style="padding:4px; border:1px solid red; color:red;">' . $error . '</div>';
+		}
+		?>
+		<div class="container">
 
-<form action="" method="post">
-	<input type="hidden" name="id" value="<?php echo $id; ?>">
-	<!-- <div class="field">
-		<div class="control">
-			<strong>ID: *</strong> <input type="text" name="firstname" value="<?php echo $id; ?>"/><br><br>
-		</div>
-	</div> -->
-	<div class="field">
-		<div class="control">
-			<strong>Favorite quote: *</strong>
-			<input type="text" name="quote" value="<?php echo $quote; ?>" /><br><br>
-		</div>
-	</div>
-	<div class="field">
-		<div class="control">
-			<strong>Name: *</strong> <input type="text" name="fullname" value="<?php echo $fullname; ?>"/><br>
-		</div>
-	</div>
-	<div class="field">
-		<div class="control">
-			<strong>Bio: *</strong> <input type="text" name="bio" class="textarea is-medium"  value="<?php echo $bio; ?>"/><br>
-		</div>
-	</div>
-	<div class="field">
-		<div class="control">
-			<strong>Link: *</strong> <input type="text" name="link" value="<?php echo $link; ?>"/><br>
-		</div>
-	</div>
-	<div>* required</div>
-	<div class="field is-grouped">
-		<div class="control">
-			<input class="button is-link" type="submit" name="submit" value="Submit">
-		</div>
-		
-		<div class="control">
-			<a href="." class="button is-link is-light">Cancel</a>
-		</div>
-	</div>
-</form>
+			<h1>Submit New Record</h1>
+			<form action="" method="post">
+				<input type="hidden" name="id" value="<?php echo $id; ?>">
+				<div class="field">
+					<strong>Favorite Quote: *</strong>
+					<input type="text" name="quote" value="<?php echo $quote; ?>" /><br>
+				</div>
 
-</div>
-</body>
-</html>
+				<div class="field">
+					<div class="control">
+						<strong>Name: *</strong>
+						<input type="text" name="fullname" value="<?php echo $fullname; ?>" /><br>
+					</div>
+				</div>
+
+				<div class="field">
+					<div class="control">
+						<strong>Bio: *</strong>
+						<input type="text" name="bio" value="<?php echo $bio; ?>" /><br>
+					</div>
+				</div>
+
+					
+				<div class="field">
+					<div class="control">
+						<strong>Website Link: *</strong> 
+						<input type="text" name="link" value="<?php echo $link; ?>" /><br>
+					</div>
+				</div>
+
+					<div>* required</div>
+					<div class="field is-grouped">
+						<div class="control">
+							<input class= button is-link" type="submit" name="submit" value="Submit">
+						</div>
+					</div>
+			</form>
+
+			<div class="control">
+				<br>
+				<a href="." class="button is-link is-light">Cancel</a>
+			</div>
+		</div>
+
+	</body>
+
+	</html>
 <?php
 }
 ?>
