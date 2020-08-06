79795743ec41c322f349a941702b3abd7263053a
Michael B <mbashner@u.rochester.edu>
2020-08-06 17:15:51 -0400

    Changed the title to project 5 and moved override to be the final css loaded

--- inc/html-top.php
+++ inc/html-top.php
@@ -4,8 +4,8 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="description" content="Project 5 Seoul Team">
-		<title><?php echo $customTitle;?> | Seoul Project 4</title>
+		<title><?php echo $customTitle;?> | Seoul Project 5</title>
 		<link rel="stylesheet" href="css/nav.css">
-		<link rel="stylesheet" href="css/override.css">
 		<link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="css/override.css">
 	</head>
