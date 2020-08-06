6b67c25c6d90b30b828006de49e353b3245c5ff6
jalhadab <67709677+jalhadab@users.noreply.github.com>
2020-08-06 21:31:35 +0300

    joined styles.css and override.css
    fml the previous VD was using 2 css files for styling

--- inc/html-top.php
+++ inc/html-top.php
@@ -6,6 +6,6 @@
 		<meta name="description" content="Project 5 Seoul Team">
 		<title><?php echo $customTitle;?> | Seoul Project 4</title>
 		<link rel="stylesheet" href="css/nav.css">
-		<link rel="stylesheet" href="css/styles.css">
+		<link rel="stylesheet" href="css/override.css">
 		<link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto&display=swap" rel="stylesheet">
 	</head>
