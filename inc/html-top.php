dcc3913f4d0578ef1c1c2d29c7342e90d2049129
Michael B <mbashner@u.rochester.edu>
2020-08-06 19:56:17 -0400

    Removed override because original override seemed to be just for the index.php page

--- inc/html-top.php
+++ inc/html-top.php
@@ -8,5 +8,5 @@
 		<link rel="stylesheet" href="css/nav.css">
 		<link rel="stylesheet" href="css/styles.css">
 		<link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto&display=swap" rel="stylesheet">
-		<link rel="stylesheet" href="css/override.css">
+		<!-- <link rel="stylesheet" href="css/override.css"> -->
 	</head>
