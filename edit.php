cc460ea86230c66392a3ad3e948bfa1287bcd3af
Michael B <mbashner@u.rochester.edu>
2020-08-07 16:48:49 -0400

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,8 +1,18 @@
 <?php
-include('renderform.php');
+// Initialize the session
+session_start();
+// Check if the user is logged in, if not then redirect them to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+	header("location: directory.php");
+	exit;
+} ?>
+<?php
+$formTitle = "Update";
+// include the form rendering function 
+include("renderform.php");
 
 // connect to the database
-include('connect-db.php');
+include("./inc/connect-db.php");
 
 // check if the form (from renderform.php) has been submitted. If it has, process the form and save it to the database
 if (isset($_POST['submit'])) {
@@ -11,21 +21,22 @@ if (isset($_POST['submit'])) {
 		// get form data, making sure it is valid
 		$id = $_POST['id'];
 		$quote = mysqli_real_escape_string($connection, htmlspecialchars($_POST['quote']));
-		$fullname = mysqli_real_escape_string($connection, htmlspecialchars($_POST['fullname']));
-		$bio = mysqli_real_escape_string($connection, htmlspecialchars($_POST['bio']));
-		$link = mysqli_real_escape_string($connection, htmlspecialchars($_POST['link']));
+		$firstname = mysqli_real_escape_string($connection, htmlspecialchars($_POST['firstname']));
+		$lastname = mysqli_real_escape_string($connection, htmlspecialchars($_POST['lastname']));
+		$about = mysqli_real_escape_string($connection, htmlspecialchars($_POST['about']));
+		$website = mysqli_real_escape_string($connection, htmlspecialchars($_POST['website']));
 
 		// check that firstname/lastname fields are both filled in
-		if ($quote == '' || $fullname == '' || $bio == '' || $link == '') {
+		if ($quote == '' || $firstname == '' || $lastname == '' || $about == '' || $website == '') {
 			// generate error message
 			$error = 'ERROR: Please fill in all required fields!';
 
 			//error, display form
-			renderForm($id, $quote, $fullname, $bio, $link, $error);
+			renderForm($id, $quote, $firstname, $lastname, $about, $website, $error, $formTitle);
 
 		} else {
 			// save the data to the database
-			$result = mysqli_query($connection, "UPDATE directory SET quote='$quote', fullname='$fullname', bio='$bio', link='$link' WHERE id='$id'");
+			$result = mysqli_query($connection, "UPDATE seoul_directory SET quote='$quote', firstname='$firstname', lastname='$lastname', about='$about', website='$website' WHERE id='$id'");
 
 			// once saved, redirect back to the homepage page to view the results
 			header("Location: directory.php");
@@ -40,19 +51,20 @@ if (isset($_POST['submit'])) {
 	if (isset($_GET['id']) && is_numeric($_GET['id']) && $_GET['id'] > 0) {
 		// query db
 		$id = $_GET['id'];
-		$result = mysqli_query($connection, "SELECT * FROM directory WHERE id=$id");
+		$result = mysqli_query($connection, "SELECT * FROM seoul_directory WHERE id=$id");
 		$row = mysqli_fetch_array( $result );
 
 		// check that the 'id' matches up with a row in the databse
 		if($row) {
 			// get data from db
 			$quote = $row['quote'];
-			$fullname = $row['fullname'];
-			$bio = $row['bio'];
-			$link = $row['link'];
+			$firstname = $row['firstname'];
+			$lastname = $row['lastname'];
+			$about = $row['about'];
+			$website = $row['website'];
 
 			// show form
-			renderForm($id, $quote, $fullname, $bio, $link, '');
+			renderForm($id, $quote, $firstname, $lastname, $about, $website, '', $formTitle);
 		} else {
 			// if no match, display result
 			echo "No results!";
