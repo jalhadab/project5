849c4d313002989fd9e270c9913c39de4ba095c4
Michael B <mbashner@u.rochester.edu>
2020-08-07 20:33:21 -0400

    Update index.php
    Use html-top.php

--- index.php
+++ index.php
@@ -87,19 +87,7 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
 }
 ?>
 <?php $customTitle="Home";?>
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="description" content="Project 5 Seoul Team">
-		<title><?php echo $customTitle;?> | Seoul Project 5</title>
-		<link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto&display=swap" rel="stylesheet">
-		<link rel="stylesheet" href="css/nav.css">
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
-		<link rel="stylesheet" type="text/css" href="css/override.css">
-	</head>
-
+<?php include "inc/html-top.php"; ?>
 	<body class="z-pattern">
 		<header class="persistent">
 			<div class = "band">
